936bf2e7113635d844b654f76a417f812ec9486c
Parag Nandi <parag7.iitr@gmail.com>
2016-04-17 12:58:07 +0530

    Added files of BruteDC

--- BruteDC/ViewController.swift
+++ BruteDC/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CTA: UIButton!
+    @IBOutlet weak var CoolBG: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func CTA(sender: AnyObject) {
+        CoolBG.hidden = false
+        CTA.hidden = true
+        
+    }
 
 }
 
